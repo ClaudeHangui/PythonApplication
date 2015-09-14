93deffef87b50d74994a363859015ea92c2671bb
hangui <leclaudio59@gmail.com>
2015-09-14 21:06:27 +0530

    fourth commit of solver file

--- src/Solver.py
+++ src/Solver.py
@@ -11,9 +11,9 @@ class Solver:
             c = int(input("c "))
             d = b ** 2 - 4 * a * c
             if d>=0:
-                discriminant = math.sqrt(d)
-                root1 = (-b + discriminant) / (2 * a)
-                root2 = (-b - discriminant) / (2 * a)
+                disc = math.sqrt(d)
+                root1 = (-b + disc) / (2 * a)
+                root2 = (-b - disc) / (2 * a)
                 print(root1, root2)
             else:
                 print('error')
